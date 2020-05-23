4c63b6b9023f532cfa9deff3519a0b1a8f053e5c
VladOSSystem <52734708+VladOSSystem@users.noreply.github.com>
2020-05-23 16:38:07 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-source-wordpress`,
     {
       resolve: "gatsby-source-wordpress",
       options: {
